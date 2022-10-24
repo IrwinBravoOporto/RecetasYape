f00e886862dc5c09fe31d4e0fd9f89259864eb74
Irwin <irwinbravooporto@gmail.com>
2022-10-23 21:25:55 -0500

    FoodListCell

--- RecetasYape/Module/Presentation/Search/View/Cell/FoodListCell.swift
+++ RecetasYape/Module/Presentation/Search/View/Cell/FoodListCell.swift
@@ -11,19 +11,30 @@ class FoodListCell: UITableViewCell {
     
     // MARK: - Items User
     @IBOutlet private weak var lblNameFood: UILabel!
-    @IBOutlet private weak var lblNameFoodDescription: UILabel!
+    @IBOutlet private weak var lblNameFoodCategory: UILabel!
+    @IBOutlet weak var imgFood: UIImageView! {
+        didSet {
+            imgFood.layer.cornerRadius = 20
+            imgFood.borderColor = .gray
+            imgFood.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var viewStart: UIView! {
+        didSet {
+            viewStart.backgroundColor = UIColor(hexString: "#D8F4D7")
+            viewStart.layer.cornerRadius = 5
+        }
+    }
     
-    @IBOutlet weak var cardView: CardView!
+    @IBOutlet weak var lblDetailCountry: UILabel!
     
-      var cornerRadiusButton: CGFloat = 20
-
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        cardView.backgroundColor = .white
-        cardView.cornerRadius = cornerRadiusButton
-        cardView.shadowColor = .gray
+    @IBOutlet weak var cardView: CardView! {
+        didSet {
+            cardView.backgroundColor = .white
+            cardView.cornerRadius = 20
+            cardView.shadowColor = .gray
+        }
     }
     
     override func prepareForReuse() {
@@ -37,12 +48,16 @@ extension FoodListCell {
     
     func setupData(data : Recipe) {
         lblNameFood.text = data.name
-        lblNameFoodDescription.text = data.description
+        lblNameFoodCategory.text = data.category
+        lblDetailCountry.text = "\(data.country ?? String()), \(data.city ?? String())"
+        imgFood.image = UIImage(named: data.imagen ?? String())
     }
     
     func clearItemsUsers() {
         lblNameFood.text = nil
-        lblNameFoodDescription.text = nil
+        lblNameFoodCategory.text = nil
+        lblDetailCountry.text = nil
+        imgFood.image = nil
     }
 }
 
